74dbac3ec8ad4bd4b377213b7478d140ed794b23
apoorvdubey23 <apoorvd23@gmail.com>
2023-08-19 19:48:01 +0530

    added carfleet

---
+++ src/component/date.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Date() {
+  return (
+    <div className='date'>
+      <form>
+        <label for='car'>choose car</label>
+       <select id='car'>
+        
+       </select>
+      </form>
+    </div>
+  )
+}
+
+export default Date
