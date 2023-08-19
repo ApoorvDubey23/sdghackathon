59ad3123577f6cb654883c8ef6ca19faf35a273a
apoorvdubey23 <apoorvd23@gmail.com>
2023-08-19 19:18:46 +0530

    adding a navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './component/navbar'
+import Landingpage from './component/landingpage'
 function App() {
   return (
     <div>
       <Navbar/>
+      <Landingpage/>
     </div>
   )
 }
---
+++ src/component/landingpage.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import POLO from '../images/poloLandingPage.jpg'
+import './landingpage.css'
+function Landingpage() {
+    return (
+        <div className='LandingPage'>
+            <div className='contentlp'>
+                <div className='cntntlp1'>
+                    Plan your trip now
+                </div>
+                <div className='cntntlp2'>
+                    Save <span className='cntntlp2big'>big</span> with our car rental
+                </div>
+                <div className='cntntlp3'>
+                    Rent the car of your dreams.Unbeatable prices,unlimited miles,flexible pick-up options and much more.
+                </div>
+                
+                <div className='cntntlp4'>
+                    <button className='button1'>Book Ride ✔ </button>
+                    <button className='button2'>Learn More</button>
+                </div>
+                </div>
+                <div className='LPimg'>
+                    <img src={POLO} alt='polo' />
+                </div>
+
+            </div>
+            )
+}
+
+            export default Landingpage
